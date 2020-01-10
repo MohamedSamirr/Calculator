eae3e7164a7220774eca9238343587ec5542626a
M.Samir <M-Samir@aucegypt.edu>
2020-01-10 19:32:40 +0200

    Comments

--- app/src/main/java/com/nopalyer/calculator/MainActivity.java
+++ app/src/main/java/com/nopalyer/calculator/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
+//cc
 import com.faendir.rhino_android.RhinoAndroidHelper;
 
 import org.mozilla.javascript.Context;
